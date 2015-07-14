0529e84628caea65801a2f2a18b18c55f12f8c92
backisbachus <jean-sebastien.renaud@larez.fr>
2015-07-14 18:46:02 +0200

    Fin essais jQuery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation();
+
+//test jQuery
+
+//$(document).ready( function() {
+//    $('#header').addClass('rouge');
+//    $('#footer').addClass('vert');
+//    $('#events').click( function() {
+//        $('.test').toggle("slow");
+//    });
+//    $('#effet1').click( function() {
+//        $('#info2').show();
+//        $('#info').hide();
+//    });
+//
+//        $('#effet2').click( function() {
+//        $('#content_footer').fadeOut();
+//        $('#footer2').fadeIn();
+//    });
+//    $('#ajax').click( function() {
+//        $('.test').addClass('cache');
+//    }); 
+//}); 
