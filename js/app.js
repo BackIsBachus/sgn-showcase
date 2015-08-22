9121423309776f0d82372657a61fe05499d49e77
backisbachus <backisbachus@backisbachus.ninja>
2015-08-22 22:45:00 +0200

    on recommence

--- js/app.js
+++ js/app.js
@@ -1,23 +1,8 @@
-$(document).foundation();
-
-//test jQuery
-
-//$(document).ready( function() {
-//    $('#header').addClass('rouge');
-//    $('#footer').addClass('vert');
-//    $('#events').click( function() {
-//        $('.test').toggle("slow");
-//    });
-//    $('#effet1').click( function() {
-//        $('#info2').show();
-//        $('#info').hide();
-//    });
-//
-//        $('#effet2').click( function() {
-//        $('#content_footer').fadeOut();
-//        $('#footer2').fadeIn();
-//    });
-//    $('#ajax').click( function() {
-//        $('.test').addClass('cache');
-//    }); 
-//}); 
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    sectionsColor: ['#FAAA34', '#FAD26E', '#FA9F81'],
+    css3: true,
+    responsiveWidth: 700
+  });
+});
