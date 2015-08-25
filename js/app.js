7386866b3e2af48d535e6a498a30350e33e29f08
backisbachus <backisbachus@backisbachus.ninja>
2015-08-26 01:57:00 +0200

    On vire fullPage et on met jQuery smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
+$(document).foundation();
 $(document).ready(function() {
-  $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', '3rdPage'],
-    sectionsColor: ['#FAAA34', '#FAD26E', '#FA9F81'],
-    css3: true,
-    responsiveWidth: 700
-  });
-});
+      $('a').smoothScroll();
+    });
